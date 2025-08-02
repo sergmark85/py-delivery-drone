5ba1e7a861799dab4b21d3f684af708ad3c89a19
Serhii Markusyk <serega.markusik@gmail.com>
2025-08-02 13:37:37 +0200

    delivery-drone2

--- app/main.py
+++ app/main.py
@@ -28,8 +28,8 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        super().__init__(name, weight, coords)
         self.coords = coords if coords is not None else [0, 0, 0]
+        super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
